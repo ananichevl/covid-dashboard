bef98e8f1e05a03e8de2ffc79cb7df46bc21d56a
Ананичев Леонид Дмитриевич <ananichev.l.dm@sberbank.ru>
2020-12-10 21:15:50 +0300

    init: fix eslint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import './scss/base.scss';
-import Table from "./js/table";
+import Table from './js/table';
 
 window.addEventListener('DOMContentLoaded', () => {
     const table = new Table('EXAMPLE TEXT');
